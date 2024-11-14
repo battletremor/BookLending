e9914951f932d13f23340cfded048b74a2fd8642
Srinivas A <battletremor@githib.com>
2024-11-14 19:03:20 +0530

    Add CORS

--- BookLending/Program.cs
+++ BookLending/Program.cs
@@ -13,6 +13,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Allow any origin
+              .AllowAnyMethod()  // Allow any HTTP method
+              .AllowAnyHeader(); // Allow any headers
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -41,7 +53,7 @@ builder.Services.AddTransient<IBookRepository, BookRepository>();
 builder.Services.AddTransient<IBookService, BookService>();
 var app = builder.Build();
 
-
+app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
 
